7789b169d8cd58d3bc2afa24727f3ea56edd315b
chrisyang2003 <1017975501@qq.com>
2022-03-29 17:51:47 +0800

    node

--- server_nodejs/config/config.default.js
+++ server_nodejs/config/config.default.js
@@ -1,24 +1,30 @@
+/* eslint valid-jsdoc: "off" */
+
 'use strict';
 
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
 module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
   const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_{{keys}}';
+  config.keys = appInfo.name + '_1648547488208_5966';
 
-  // add your config here
+  // add your middleware config here
   config.middleware = [];
 
-  // change to your own sequelize configurations
-  // config.sequelize = {
-  //   dialect: 'mysql',
-  //   host: 'localhost',
-  //   port: 3306,
-  //   database: 'egg-sequelize-default',
-  //   username: 'root',
-  //   password: '',
-  // };
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
 
-
-  return config;
+  return {
+    ...config,
+    ...userConfig,
+  };
 };
--- server_nodejs/config/plugin.js
+++ server_nodejs/config/plugin.js
@@ -1,6 +1,9 @@
 'use strict';
 
-exports.sequelize = {
-  enable: true,
-  package: 'egg-sequelize',
+/** @type Egg.EggPlugin */
+module.exports = {
+  // had enabled by egg
+  // static: {
+  //   enable: true,
+  // }
 };
